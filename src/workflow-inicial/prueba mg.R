d29b005a81e62da97b097bc7a92f367856a6db25
magiord <magiord001@gmail.com>
2023-07-13 15:46:45 +0000

    commit0

---
+++ src/workflow-inicial/prueba mg.R
@@ -0,0 +1,33 @@
+# data drift descriptiva
+
+# limpio la memoria
+rm(list = ls(all.names = TRUE)) # remove all objects
+gc(full = TRUE) # garbage collection
+
+require("data.table")
+require("yaml")
+
+
+# Parametros del script
+PARAM <- list()
+PARAM$experimento <- "DR6210"
+
+PARAM$exp_input <- "CA6110"
+
+setwd(PARAM$home)
+
+# cargo el dataset donde voy a entrenar
+# esta en la carpeta del exp_input y siempre se llama  dataset.csv.gz
+dataset_input <- paste0("./exp/", PARAM$exp_input, "/dataset.csv.gz")
+dataset <- fread(dataset_input)
+
+# Calcula la densidad de sueldo para cada mes
+density_data <- dataset[, list(Density = density(mpayrool)), by = foto_mes]
+# Gráfica la densidad de sueldo para cada mes
+ggplot(density_data, aes(x = x, y = y, color = Mes)) +
+  geom_line() +
+  labs(x = "Sueldo", y = "Densidad", title = "Densidad de Sueldo por Mes") +
+  theme_minimal()
+
+
+
